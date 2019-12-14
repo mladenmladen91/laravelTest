f526c97fb184d9f2d3de48ac3c6774bd5663a007
vasko <vjelovac90@gmail.com>
2019-12-14 19:49:16 +0100

    mend

--- laravel/database/seeds/DatabaseSeeder.php
+++ laravel/database/seeds/DatabaseSeeder.php
@@ -41,18 +41,6 @@ class DatabaseSeeder extends Seeder
         User::insert($data);
         Note::insert($noteData);
         
-      /*  $data4 = [];
-        $noteData3 = [];
-        
-        for($i = 1;$i < 5001; $i++){
-            $data1 = ['first_name'=>$faker->firstName,'last_name'=>$faker->lastName,'avatar'=>'admin.png','zip'=>'567','phone'=>$faker->phoneNumber,'country'=>'Montenegro','address'=>$faker->address,'group_id'=>$kategorija, 'email'=>$faker->unique()->safeEmail];
-            array_push($data4, $data1);
-            
-            $data2 = ['name'=>'test text','user_id'=>$j + $i];
-            array_push($noteData3, $data2);
-        }
-        User::insert($data4);
-        Note::insert($noteData3);*/
         
     }
 }
